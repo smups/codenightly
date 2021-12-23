0a908f94d0633a7c73f009810dd17cd095a973e8
Raúl Wolters <56522208+smups@users.noreply.github.com>
2021-12-23 15:55:55 +0100

    port conventie toegevoegd aan codenightly

---
+++ src/main/java/com/smups/codenightly/Conventions.java
@@ -0,0 +1,15 @@
+package com.smups.codenightly;
+
+public interface Conventions {
+    /**
+     * Deze interface bevat _alleen_ constantes en conventies
+     * 
+     * @author Raúl
+     */
+
+
+    /**
+     * Port die gebruikt wordt voor communicatie tussen de server en de clients
+     */
+    public static int CODENIGHTLY_PORT = 8888;
+}
